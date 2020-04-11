434a555ae7ce9cbc5c95f6b821ce24b6287de9a9
spencer <spencer.takata@gmail.com>
2020-04-11 13:28:59 -0700

    la + templating work

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,13 @@ const nunjucksRender = require('gulp-nunjucks-render');
 
 function nunjucks() {
     //Get .html and .njk files in pages folder
-    return gulp.src('stage/*.njk')
+    return gulp.src('./deploy/stage/*.njk')
     // Render template with nunjucksRender
         .pipe(nunjucksRender({
             path: ['./templates']
         }))
-    // output files in public folder
-    .pipe(gulp.dest('./'))
+    // output files in deploy folder
+    .pipe(gulp.dest('./deploy'))
 };
 
 exports.default = nunjucks;
